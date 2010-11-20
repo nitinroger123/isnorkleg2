670d0a2ba6cc418e0cb8ed223ec20061113015bd
lep2128 <lep2128@22f42647-f149-18a4-4a01-21e0d5e00428>
2010-11-20 04:39:06 +0000

    Added skeleton method for divers to return to center

--- trunk/src/isnork/g2/JustKeepSwimming.java
+++ trunk/src/isnork/g2/JustKeepSwimming.java
@@ -4,7 +4,10 @@ import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+
 import isnork.sim.GameConfig;
+import isnork.sim.GameController;
 import isnork.sim.Observation;
 import isnork.sim.Player;
 import isnork.sim.SeaLife;
@@ -17,8 +20,13 @@ public class JustKeepSwimming extends Player {
 	private Direction direction;
 	private Guidebook guidebook;
 	private Point2D whereIAm = null;
+	private Point2D boat = new Point2D.Double(0, 0);
 	private int n = -1;
-	private int radius, distance, penalty, numrounds;
+	private int radius, distance, penalty, numrounds, roundsleft;
+	private double boatConstant = 1.2;
+	private Logger log;
+
+	
 
 	private Direction getNewDirection() {
 		int r = random.nextInt(100);
@@ -38,7 +46,11 @@ public class JustKeepSwimming extends Player {
 	public String tick(Point2D myPosition, Set<Observation> whatYouSee,
 			Set<iSnorkMessage> incomingMessages,
 			Set<Observation> playerLocations) {
+		
+		//Update variables
 		whereIAm = myPosition;
+		roundsleft --;
+		
 		if (n % 10 == 0)
 			return "s";
 		else
@@ -47,6 +59,10 @@ public class JustKeepSwimming extends Player {
 
 	@Override
 	public Direction getMove() {
+		
+		if(whereIAm.distance(boat) < boatConstant * roundsleft)
+			return backtrack();
+		
 		return dumbMove();
 	}
 
@@ -64,11 +80,30 @@ public class JustKeepSwimming extends Player {
 		}
 		return d;
 	}
+	
+	/**Move to get the player back to the boat*/
+	public Direction backtrack() {
+		log.trace("backtracking");
+		Direction d = getNewDirection();
+
+		Point2D p = new Point2D.Double(whereIAm.getX() + d.dx, whereIAm.getY()
+				+ d.dy);
+		while (Math.abs(p.getX()) > GameConfig.d
+				|| Math.abs(p.getY()) > GameConfig.d) {
+			d = getNewDirection();
+			p = new Point2D.Double(whereIAm.getX() + d.dx, whereIAm.getY()
+					+ d.dy);
+		}
+		return d;
+	}
 
 	/** Initialize our variables when a new game is created */
 	@Override
 	public void newGame(Set<SeaLifePrototype> seaLifePossibilites, int p,
 			int d, int r, int n) {
+		
+		//Initialize game variables
+		log = Logger.getLogger(this.getClass());
 
 		guidebook = new Guidebook();
 		for (SeaLifePrototype s : seaLifePossibilites) {
@@ -79,6 +114,7 @@ public class JustKeepSwimming extends Player {
 		distance = d;
 		radius = r;
 		numrounds = n;
+		roundsleft = n;
 	}
 
 }
