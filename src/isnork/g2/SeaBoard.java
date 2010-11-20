5a867c4f9d89a75c4815d4b05b620b0c1af0ebf4
lep2128 <lep2128@22f42647-f149-18a4-4a01-21e0d5e00428>
2010-11-20 20:09:44 +0000

    Working on avoiding dangerous moves

--- src/isnork/g2/SeaBoard.java
+++ src/isnork/g2/SeaBoard.java
@@ -1,20 +1,29 @@
 package isnork.g2;
 
 import isnork.sim.Observation;
-
+import isnork.sim.SeaLifePrototype;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Set;
+
+import isnork.sim.GameObject.Direction;
 
 public class SeaBoard {
 	
-	public SeaBoard(int x, int y){
+	public SeaBoard(int x, int y, int r, Set<SeaLifePrototype> p, int d){
 		creatures = new ArrayList<SeaCreature>();
+		prototypes = p;
 		board = new SeaSpace[x][y];
+		for(int i = 0; i < x; i++){
+			for(int j = 0; j < y; j++){
+				board[i][j] = new SeaSpace(new Point2D.Double(i, j));
+			}
+		}
+				
+		radius = r;
+		distance = d;
 	}
-	
-	public void add(SeaCreature s){
-		creatures.add(s);
-	}
-	
+
 	public void remove(int id){
 		
 		for(int i = 0; i < board.length; i++){
@@ -26,18 +35,61 @@ public class SeaBoard {
 		}
 	}
 	
-	public void add(Observation o) {
-		
-		SeaCreature temp;
-		
+	public void add(Observation o, int r) {
+				
+		Boolean found = false;
 		for(SeaCreature c: creatures){
 			if(c.getId() == o.getId()){
-				board[(int) o.getLocation().getX()][(int) o.getLocation().getY()].addCreature(c);
+				board[(int) o.getLocation().getX() + distance]
+				      [(int) o.getLocation().getY() + distance].addCreature(c, r);
+				found = true;
 			}
 		}
 		
+		if(!found){
+			for(SeaLifePrototype p: prototypes){
+				if(p.getName() == o.getName()){
+					creatures.add(new SeaCreature(p, o.getId(), r));
+				}
+			}
+		}
+	}
+	
+	/**Determines if there is a dangerous animal within the radius*/
+	public boolean getDangerInRadius(Point2D me, int r){
+		
+		for(int i = 0;i < board.length; i++){
+			for(int j = 0; j < board[0].length; j++){
+				if(board[i][j].getPoint().distance(me) <= radius){
+					if(board[i][j].hasDanger(r)){
+						return true;
+					}
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	public ArrayList<Direction> getDangerousDirections(Point2D me, int r) {
+		ArrayList<Direction> d = new ArrayList<Direction>();
+		
+		for(int i = 0;i < board.length; i++){
+			for(int j = 0; j < board[0].length; j++){
+				if(board[i][j].getPoint().distance(me) <= radius){
+					if(board[i][j].hasDanger(r)){
+						d.add(board[i][j].getDirection(me));
+					}
+				}
+			}
+		}
+		
+		return d;
 	}
 	
 	private ArrayList<SeaCreature> creatures;
+	private Set<SeaLifePrototype> prototypes;
 	private SeaSpace[][] board;
+	private int radius, distance;
+	
 }
